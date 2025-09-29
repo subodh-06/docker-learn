cb855c11a9879190634e3fec6a120d9964a83be9
subodh-06 <subodhkumarpradhan63@gmail.com>
2025-09-29 22:44:50 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+import 'dotenv/config'
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
